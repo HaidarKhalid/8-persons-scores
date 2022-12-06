62107a42ef45a33a9f3c56b9228d5a914361c9c5
Haidar Khalid <110727504+HaidarKhalid@users.noreply.github.com>
2022-12-06 16:33:30 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 let inpEl = document.querySelector(".inp-num").value
+
 let persons;
+
 let root = document.getElementById("root")
 
+let inpu = document.querySelector(".inp-num")
 
+inpu.addEventListener("keydown", (e)=> {
+    if (e.key == "Enter") {
+        show()
+    } else {
+        return
+    }
+})
+class Player {
+    constructor(score, number) {
+        this.score = score
+        this.number = number
+    }
+}
 function show() {
     inpEl = document.querySelector(".inp-num").value
     if (inpEl > 8) {
@@ -12,36 +28,65 @@ function show() {
     } else {
         persons = inpEl
     }
+
     root.innerHTML = ""
+
     addPlayers()
 }
-var added = 0;
-var score =  [];
+
+
+
+let added = 0;
+let x = []
 function addPlayers() {
+
+
     if (persons > added) {
+
     added++
+        
+    x.push(new Player(0, added))
+
         root.innerHTML += `
         <div class="player">
-            <h3>player ${added}</h3>
-            <p >score: <span id="score${added}">0</span></p>
-            <button class="inc" onclick="add(${added})">Increase</button>
-            <button class="dec" onclick="minus(${added})">Decrease</button>
-            <button class="reset" onclick="reset(${added})">Reset</button>
+            <h3>player ${x[added - 1].number}</h3>
+            <p >score: <span id="score${x[added - 1].number}">0</span></p>
+            <button class="inc" onclick="add(${x[added - 1].number})">Increase</button>
+            <button class="dec" onclick="minus(${x[added - 1].number})">Decrease</button>
+            <button class="reset" onclick="reset(${x[added - 1].number})">Reset</button>
         </div>`
-    score.push(0)
-    addPlayers() 
+        
+
+        addPlayers() 
+
     } else {
-        console.log(added)
-        console.log(score)
+        console.log(x)
+        root.innerHTML += `
+        <br>
+        <br>
+        <br>
+        <button onclick="refresh()" class="refresh-btn"><span class="refresh-text">Main menu</span></button>
+        `
     }
 }
 
+
+
+
+
+
+
+
 function add(num) {
-     document.getElementById(`score${num}`).innerText = score[num - 1] += 1
+     document.getElementById(`score${num}`).innerText = x[num - 1].score += 1
 }
 function minus(num) {
-     document.getElementById(`score${num}`).innerText = score[num - 1] -= 1
+     document.getElementById(`score${num}`).innerText = x[num - 1].score -= 1
 }
 function reset(num) {
-    document.getElementById(`score${num}`).innerText = score[num - 1] = 0
+    document.getElementById(`score${num}`).innerText = x[num - 1].score = 0
+}
+
+function refresh() {
+    location.reload()
 }
